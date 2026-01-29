070b1f2737e6f729ac99b7d3cca72121ab872499
becape187 <bernardo@becape.com.br>
2026-01-28 22:19:32 -0300

    ..

--- Motores.IO.server/Motores.IO.server.API/Program.cs
+++ Motores.IO.server/Motores.IO.server.API/Program.cs
@@ -31,9 +31,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
